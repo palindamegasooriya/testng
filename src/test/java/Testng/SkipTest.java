b6a24b02eb07dc710c675bb18ecfaabcb488e5a6
User <palindamit@gmail.com>
2021-12-05 10:15:45 +0530

    Skip test with skipException

--- src/test/java/Testng/SkipTest.java
+++ src/test/java/Testng/SkipTest.java
@@ -1,10 +1,17 @@
 package Testng;
 
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 public class SkipTest {
     @Test(enabled = true)
     public void skiptest1() {
         System.out.println("skipping this test as it is not complete");
+    }
+
+    @Test
+    public void skiptest2() {
+        System.out.println("skipping this test forcefully");
+        throw new SkipException("Skip this test");
     }
 }
