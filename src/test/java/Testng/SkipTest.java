2359a1116ad26731252517143ef9c97341ba3ffc
User <palindamit@gmail.com>
2021-12-05 10:23:14 +0530

    With skiptest3 data setup

--- src/test/java/Testng/SkipTest.java
+++ src/test/java/Testng/SkipTest.java
@@ -4,6 +4,9 @@ import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 public class SkipTest {
+
+    boolean datasetup=true;
+
     @Test(enabled = true)
     public void skiptest1() {
         System.out.println("skipping this test as it is not complete");
@@ -14,4 +17,21 @@ public class SkipTest {
         System.out.println("skipping this test forcefully");
         throw new SkipException("Skip this test");
     }
+
+    @Test
+    public void skiptest3() {
+        System.out.println("skipping based on condition");
+
+        if(datasetup=false)
+        {
+            System.out.println("Execute the test");
+        }
+        else
+        {
+            System.out.println("do not execute further steps");
+            throw new SkipException("do not execute further steps");
+        }
+
+    }
+
 }
