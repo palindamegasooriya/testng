63bfa40c1f9c645d52d3613599aeb73680649c74
User <palindamit@gmail.com>
2021-12-11 10:22:12 +0530

    Retry methods

---
+++ src/test/java/Testng/Retryeffect.java
@@ -0,0 +1,13 @@
+package Testng;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Retryeffect {
+
+    @Test(retryAnalyzer = Retry.class)
+    public void retry() {
+        System.out.println("Hello");
+        Assert.assertTrue(false);
+    }
+}
